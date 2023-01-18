70624d5b75be474891a0f04dfe674d48698f835a
SamuelArico99 <samuel16699.sa@gmail.com>
2023-01-18 16:18:56 +0100

    struttura html css

--- script.js
+++ script.js
@@ -1,61 +1,26 @@
 const images = [
-    "img/01.webp",
-    "img/02.webp",
-    "img/03.webp",
-    "img/04.webp",
-    "img/05.webp"
-]
-const carouselElement = document.querySelector('.carousel');
-
-
-for (let index = 0; index < images.length; index++) {
-    console.log(images[index]);
-
-    carouselElement.innerHTML +=  ` <div class="slide">
-                                         <img src="${images[index]}" alt="">
-                                    </div> ` 
-    
-}
-
-const allSlides = document.querySelectorAll('.slide');
-console.log('allSlide' , allSlides)
-allSlides[0].classList.add('current');
-
+  {
+      image: 'img/01.webp',
+      title: 'Marvel\'s Spiderman Miles Morale',
+      text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+  }, {
+      image: 'img/02.webp',
+      title: 'Ratchet & Clank: Rift Apart',
+      text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+  }, {
+      image: 'img/03.webp',
+      title: 'Fortnite',
+      text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+  }, {
+      image: 'img/04.webp',
+      title: 'Stray',
+      text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+  }, {
+      image: 'img/05.webp',
+      title: "Marvel's Avengers",
+      text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+  }
+];
+const slidesContainer = document.querySelector('.slides');
+const thumbnailsContainer = document.querySelector('.thumbnails')
 let currentSlide = 0;
-
-const nextArrow = document.querySelector('.next');
-
-nextArrow.addEventListener('click',
-
-      function () {
-
-        allSlides[currentSlide].classList.remove('current');
-        
-        if (currentSlide == allSlides.length -1) {
-          currentSlide = 0;
-        }
-        else{
-          currentSlide = currentSlide + 1;
-        }
-
-        allSlides[currentSlide].classList.add('current');
-      }
- 
-);
-
-const prevArrow = document.querySelector('.previous');
-
-prevArrow.addEventListener('click',
-
-      function () {
-        allSlides[currentSlide].classList.remove('current');
-        if (currentSlide == 0) {
-          currentSlide = allSlides.length - 1;
-        }
-        else{
-          currentSlide = currentSlide - 1;
-        }
-        allSlides[currentSlide].classList.add('current');
-      }
- 
-);
