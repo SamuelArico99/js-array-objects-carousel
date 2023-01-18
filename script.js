79f5b4e56a1a1177ff54fc0016365eb05a02d107
SamuelArico99 <samuel16699.sa@gmail.com>
2023-01-18 17:51:34 +0100

    Milestone 1

--- script.js
+++ script.js
@@ -24,3 +24,80 @@ const images = [
 const slidesContainer = document.querySelector('.slides');
 const thumbnailsContainer = document.querySelector('.thumbnails')
 let currentSlide = 0;
+
+images.forEach((singleImage) =>{
+  console.log(singleImage);
+  const newSlide = document.createElement('div');
+  newSlide.classList.add('slide');
+  newSlide.innerHTML =
+   `
+  <img src="${singleImage.image}" alt="">
+  <div class="texts">
+    <h3>
+      ${singleImage.title}
+    </h3>
+    <p>
+      ${singleImage.text}
+    </p>
+  </div>
+  `;
+
+ slidesContainer.append(newSlide);
+
+ const newThumbnail = document.createElement('div');
+ newThumbnail.classList.add('thumbnail');
+ newThumbnail.style.height = (100 / images.length) + '%';
+ newThumbnail.innerHTML =
+  `
+ <img src="${singleImage.image}" alt="">
+
+ `;
+
+thumbnailsContainer.append(newThumbnail);
+
+});
+
+const allSlides = document.querySelectorAll('.slide');
+const allThumbnails = document.querySelectorAll('.thumbnail');
+
+allSlides[currentSlide].classList.add('active');
+allThumbnails[currentSlide].classList.add('active');
+
+const prevButton = document.querySelector('.prev');
+prevButton.addEventListener('click',
+     () => {
+
+      if (currentSlide > 0) {
+        allSlides[currentSlide].classList.remove('active');
+        allThumbnails[currentSlide].classList.remove('active');
+
+        currentSlide--;
+
+        allSlides[currentSlide].classList.add('active');
+        allThumbnails[currentSlide].classList.add('active');
+      }
+      else{
+
+      }
+     }
+);
+
+const nextButton = document.querySelector('.next');
+nextButton.addEventListener('click',
+     () => {
+
+      if (currentSlide < images.length -1) {
+        allSlides[currentSlide].classList.remove('active');
+        allThumbnails[currentSlide].classList.remove('active');
+
+        currentSlide++;
+
+        allSlides[currentSlide].classList.add('active');
+        allThumbnails[currentSlide].classList.add('active');
+      }
+      else{
+
+      }
+     }
+);
+
